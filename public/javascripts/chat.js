21ba67862083ea1a711d09038e5e88443d145606
Grey Delamar <Grey@Greys-MacBook-Air.local>
2016-08-26 15:45:01 -0600

    fixed a few things

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -7,12 +7,9 @@ $(function(){
   $('form').submit(function(f){
     socket.emit('message', $('#messaged').val());
     f.preventDefault();
-    $('#messaged').val('');
   });
   socket.on('message', function(msg){
     $('#messages').append($('<div>').text(msg));
-    });
-   $('#setname').click(function(){
-   socket.emit("set_name", {name: $('#nickname').val()});
-   });
+  });
+
 })
--- socket.js
+++ socket.js
@@ -2,11 +2,8 @@ var io = require('socket.io')();
 
 io.on("connection", function(socket){
   console.log('a user connected');
-  // socket.on("message", function(data){
-  //   console.log(data)
-  // })
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    console.log('a user disconnected');
   });
 });
 
