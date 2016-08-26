b1937c610cc58c866ad173315b66976f5973a7bb
Grey Delamar <Grey@Greys-MacBook-Air.local>
2016-08-26 13:47:50 -0600

    trying to fix clear and input maxlength

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -7,9 +7,9 @@ $(function(){
   $('form').submit(function(f){
     socket.emit('message', $('#messaged').val());
     f.preventDefault();
+    $('#messaged').val('');
   });
   socket.on('message', function(msg){
-        $('#messages').append($('<li>').text(msg));
-        $('#messaged').val('');
-      });
+    $('#messages').append($('<li>').text(msg));
+    });
 })
