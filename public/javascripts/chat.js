c5157e8cb070f5b7a059e31b7eb4f95b0d74684b
Grey Delamar <Grey@Greys-MacBook-Air.local>
2016-08-26 15:48:28 -0600

    fixed input feild clearing

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -7,6 +7,7 @@ $(function(){
   $('form').submit(function(f){
     socket.emit('message', $('#messaged').val());
     f.preventDefault();
+    $('#messaged').val('');
   });
   socket.on('message', function(msg){
     $('#messages').append($('<div>').text(msg));
